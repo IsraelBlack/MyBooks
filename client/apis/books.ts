86e9afdb400d636fb8d16d077fe224cd97beade6
Israel Black <israelblack98@outlook.com>
2023-08-29 11:17:12 +1200

    Added Update API

--- client/apis/books.ts
+++ client/apis/books.ts
@@ -2,6 +2,7 @@ import request from 'superagent'
 import { BookData } from '../../models/types'
 
 const booksURL = 'api/v1/books'
+const req = request
 
 export async function fetchBooks() {
   const res = await request.get(booksURL)
@@ -15,10 +16,14 @@ export async function fetchOneBook(id: number) {
 }
 
 export async function remove(id: number) {
-  await request.delete(`${booksURL}/${id}`)
+  await req.delete(`${booksURL}/${id}`)
 }
 
 export async function postBook(book: BookData) {
   const res = await request.post(booksURL).send(book)
   return res.body
+}
+
+export async function updateRating(id: number, newRating: number) {
+  await req.patch(`${booksURL}/${id}`).send({rating: newRating})
 }
