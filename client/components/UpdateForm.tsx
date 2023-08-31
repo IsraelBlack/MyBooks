fbc8b3e6c7b7e2a87324f7ed7da9122707c4dd69
Israel Black <israelblack98@outlook.com>
2023-08-31 16:35:59 +1200

    Created UF component

---
+++ client/components/UpdateForm.tsx
@@ -0,0 +1,5 @@
+import { ChangeEvent, useState, FormEvent } from 'react'
+import { useAppDispatch } from '../hooks/hooks'
+import { BookData } from '../../models/types'
+import * as actions from '../actions/books'
+import { Button } from '@mui/material'
