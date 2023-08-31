06b48532f8ca140ed54c15d53fdb1cc8d7551672
Israel Black <israelblack98@outlook.com>
2023-08-31 16:41:03 +1200

    created usestate

--- client/components/UpdateForm.tsx
+++ client/components/UpdateForm.tsx
@@ -3,3 +3,13 @@ import { useAppDispatch } from '../hooks/hooks'
 import { BookData } from '../../models/types'
 import * as actions from '../actions/books'
 import { Button } from '@mui/material'
+
+export default function UpdateForm() {
+  const dispatch = useAppDispatch()
+  const [formData, setFormData] = useState({} as BookData)
+
+  //todo create handleChange
+  //todo create handleSubmit
+
+  //todo create form 
+}
