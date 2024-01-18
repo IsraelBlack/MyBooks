668703cc3b49af9ee1afc3c126430d677bea295e
Israel Black <israelblack98@outlook.com>
2024-01-18 14:34:21 +1300

    render books

--- client/features/books/booksList.tsx
+++ client/features/books/booksList.tsx
@@ -1,5 +1,18 @@
-import { useAppSelector } from "../../hooks/hooks";
+import { Key } from 'react'
+import { useAppSelector } from '../../hooks/hooks'
 
 const BooksList = () => {
-  const books = useAppSelector(state=>state.books)
+  const books = useAppSelector((state) => state.books)
+
+  const renderedBooks = books.map(
+    (books: { id: number; title: string; author: string; summary: string }) => (
+      <article key={books.id}>
+        <h3>{books.title}</h3>
+        <h4>{books.author}</h4>
+        <p>{books.summary.substring(0, 100)}</p>
+      </article>
+    ),
+  )
 }
+
+export default BooksList
