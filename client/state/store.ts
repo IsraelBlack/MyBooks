71dab70eed47f739ec55c878f49c6ba0d19f9076
Israel Black <israelblack98@outlook.com>
2024-01-18 14:15:33 +1300

    import booksReducer

--- client/state/store.ts
+++ client/state/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
+import booksReducer from '../features/books/bookSlice'
 
 export const store = configureStore({
   reducer: {}, //TODO Add reducers once made
