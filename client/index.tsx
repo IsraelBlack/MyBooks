85d3e77529bdff33751639981c27c3bf7dc288b0
Israel Black <israelblack98@outlook.com>
2024-01-18 14:55:57 +1300

    update index

--- client/index.tsx
+++ client/index.tsx
@@ -1,13 +1,15 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { store } from './state/store'
 import { Provider } from 'react-redux'
-import store from './store'
 
-import App from './components/App'
-
-document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('app') as HTMLElement).render(
+ReactDOM.render(
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  )
-})
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
