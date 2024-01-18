0b38a1df479278d7dc809be779a2512cbcec6a87
Israel Black <israelblack98@outlook.com>
2024-01-18 14:15:53 +1300

    implement reducer

--- client/state/store.ts
+++ client/state/store.ts
@@ -2,7 +2,9 @@ import { configureStore } from '@reduxjs/toolkit'
 import booksReducer from '../features/books/bookSlice'
 
 export const store = configureStore({
-  reducer: {}, //TODO Add reducers once made
+  reducer: {
+    books: booksReducer
+  }, //TODO Add reducers once made
 })
 
 export type RootState = ReturnType<typeof store.getState>
