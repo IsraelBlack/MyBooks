5c97dcaab8ba8dddf7021cf8e7d014bebaa938b0
Israel Black <israelblack98@outlook.com>
2024-01-18 16:19:41 +1300

    updated id

--- client/features/books/bookSlice.tsx
+++ client/features/books/bookSlice.tsx
@@ -1,13 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { Book } from '../../../models/types'
 
+//const initialState = [] as Book[]
 
-const initialState = [] as Book[]
+const initialState = [
+  {
+    id: '1',
+    title: 'Learning Redux Toolkit',
+    content: "I've heard good things.",
+  },
+  {
+    id: '2',
+    title: 'Slices...',
+    content: 'The more I say slice, the more I want pizza.',
+  },
+]
 
 const bookSlice = createSlice({
   name: 'books',
   initialState,
-  reducers: {}
+  reducers: {},
 })
 
 export const selectAllBooks = (state: { books: any }) => state.books
--- client/features/books/booksList.tsx
+++ client/features/books/booksList.tsx
@@ -4,13 +4,22 @@ import { selectAllBooks } from './bookSlice'
 const BooksList = () => {
   const books = useAppSelector(selectAllBooks)
 
+  // const renderedBooks = books.map(
+  //   (books: { id: number; title: string; author: string; summary: string }) => (
+  //     <article key={books.id}>
+  //       <h2>Test For Display</h2>
+  //       <h3>{books.title}</h3>
+  //       <h4>{books.author}</h4>
+  //       <p>{books.summary.substring(0, 100)}</p>
+  //     </article>
+  //   ),
+  // )
+
   const renderedBooks = books.map(
-    (books: { id: number; title: string; author: string; summary: string }) => (
-      <article key={books.id}>
-        <h2>Test For Display</h2>
-        <h3>{books.title}</h3>
-        <h4>{books.author}</h4>
-        <p>{books.summary.substring(0, 100)}</p>
+    (book: { id: number; title: string; content: string }) => (
+      <article key={book.id}>
+        <h3>{book.title}</h3>
+        <p>{book.content.substring(0, 100)}</p>
       </article>
     ),
   )
