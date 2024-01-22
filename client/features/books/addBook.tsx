b2c21e9c263aedf12d1b63e1c50af65573aaf688
Israel Black <israelblack98@outlook.com>
2024-01-23 12:19:36 +1300

    add bookform

---
+++ client/features/books/addBook.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import React from 'react'
+
+function addBookForm() {
+  const [title, setTitle] = useState('')
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Form
