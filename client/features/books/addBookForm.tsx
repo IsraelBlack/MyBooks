9489840e07bc6c30a35beba4d052a862acf49b97
Israel Black <israelblack98@outlook.com>
2024-01-24 11:12:28 +1300

    onChange Actions

--- client/features/books/addBookForm.tsx
+++ client/features/books/addBookForm.tsx
@@ -1,11 +1,34 @@
 /* Book Form */
 
-import {useState} from "react"
+import {SetStateAction, useState} from "react"
 
 
 const AddBookForm = () => {
   const [title, setTitle] = useState('')
   const [summary, setSummary] = useState('')
+
+  //TODO Look for shorter way to achieve this
+  const onTitleChanged = (e: { target: { value: SetStateAction<string> } }) => setTitle(e.target.value)
+  const onSummaryChanged = (e: { target: { value: SetStateAction<string> } }) => setSummary(e.target.value)
+
+  //? This is to test long and short text input
+  //TODO add all other book items once working!
+
+  return (
+    <section>
+      <h2>Add a Book!</h2>
+      <form>
+        <label htmlFor="bookTitle">Book Title: </label>
+        <input 
+         type="text"
+         id="bookTitle"
+         name="bookTitle"
+         value={title}
+         onChange={onTi}
+         />
+      </form>
+    </section>
+  )
 }
 
 export default AddBookForm
