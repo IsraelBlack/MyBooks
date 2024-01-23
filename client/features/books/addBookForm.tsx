1ea0878878295e6bc3f7c8bfdf39a9cf9a584385
Israel Black <israelblack98@outlook.com>
2024-01-24 11:16:21 +1300

    html added

--- client/features/books/addBookForm.tsx
+++ client/features/books/addBookForm.tsx
@@ -1,15 +1,16 @@
 /* Book Form */
 
-import {SetStateAction, useState} from "react"
-
+import { SetStateAction, useState } from 'react'
 
 const AddBookForm = () => {
   const [title, setTitle] = useState('')
   const [summary, setSummary] = useState('')
 
   //TODO Look for shorter way to achieve this
-  const onTitleChanged = (e: { target: { value: SetStateAction<string> } }) => setTitle(e.target.value)
-  const onSummaryChanged = (e: { target: { value: SetStateAction<string> } }) => setSummary(e.target.value)
+  const onTitleChanged = (e: { target: { value: SetStateAction<string> } }) =>
+    setTitle(e.target.value)
+  const onSummaryChanged = (e: { target: { value: SetStateAction<string> } }) =>
+    setSummary(e.target.value)
 
   //? This is to test long and short text input
   //TODO add all other book items once working!
@@ -18,14 +19,23 @@ const AddBookForm = () => {
     <section>
       <h2>Add a Book!</h2>
       <form>
+        {/* Book Title */}
         <label htmlFor="bookTitle">Book Title: </label>
-        <input 
-         type="text"
-         id="bookTitle"
-         name="bookTitle"
-         value={title}
-         onChange={onTi}
-         />
+        <input
+          type="text"
+          id="bookTitle"
+          name="bookTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        {/* Book Summary*/}
+        <label htmlFor="bookTitle">Book Title: </label>
+        <textarea
+          id="bookSummary"
+          name="bookSummary"
+          value={summary}
+          onChange={onSummaryChanged}
+        />
       </form>
     </section>
   )
